72fa3da3114dc113d6f109b743908a5f916a6a6a
brandonklotz <brandonklotz@icloud.com>
2019-05-09 17:13:02 -0400

    Add new logo component and remove old one

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import { goToHome } from "../actions";
 import { connect } from "react-redux";
 import "font-awesome/css/font-awesome.css";
+import Logo from './logo/logo.js';
 
 class Header extends Component {
     render() {
         return (
             <header>
                 <div className="logo" onClick={this.props.goToHome}>
-                    <h1>Type to Tone</h1>
+                    <Logo />
+                    <h1>Type To Tone</h1>
                 </div>
             </header>
         );
---
+++ client/src/components/logo/logo.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import './logo.css'
+
+class Logo extends Component {
+    render() {
+        return (
+            <svg width="38px" height="38px" viewBox="0 0 38 38">
+                <defs>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-1">
+                        <stop stop-color="#FFE7EC" offset="0%"></stop>
+                        <stop stop-color="#FFCAD4" offset="100%"></stop>
+                    </linearGradient>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-2">
+                        <stop stop-color="#FC1A69" offset="0%"></stop>
+                        <stop stop-color="#F70B38" offset="100%"></stop>
+                    </linearGradient>
+                </defs>
+                <g id="Welcome" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="Mobile-Portrait" transform="translate(-22.000000, -19.000000)">
+                        <g id="Group" transform="translate(22.000000, 19.000000)">
+                            <circle id="Oval" fill="url(#linearGradient-1)" cx="17" cy="21" r="17"></circle>
+                            <circle id="Oval-Copy" stroke="url(#linearGradient-2)" cx="20" cy="18" r="17"></circle>
+                            <path d="M12.5,21.5 L12.5,15.5" id="Path" stroke="#F91252" stroke-linecap="round"></path>
+                            <path d="M20.5,25.5 L20.5,11.5" id="Path-Copy" stroke="#F91252" stroke-linecap="round"></path>
+                            <path d="M28.5,23.5 L28.5,13.5" id="Path-Copy-2" stroke="#F91252" stroke-linecap="round"></path>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        );
+    }
+}
+
+export default Logo;
